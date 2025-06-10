3f75e1cd7c24812e79978c15ae6b48356259a05e
autumn-ahreum <hanjiro77@gmail.com>
2025-06-10 11:01:53 -0700

    changes using context

---
+++ books/src/components/BookList.js
@@ -0,0 +1,16 @@
+import BookShow from './BookShow';
+import useBooksContext from '../hooks/use-books-context';
+
+
+function BookList() {
+    const { books }  = useBooksContext();
+
+    const renderedBooks = books.map((book)=>{
+        return <BookShow key={book.id} book={book} />;
+    });
+    console.log('books is', books);
+    return <div class Name="book-list">
+        {renderedBooks}
+        </div>;
+}
+export default BookList;
